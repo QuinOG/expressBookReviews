f50d780b121993bad9c702fd724a598c86d6dfc9
QuinOG <quinlandavis0010@gmail.com>
2023-05-28 19:03:13 -0400

    Update general.js
    added public user routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,34 +10,40 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Book List
+
+public_users.get('/', function(req,res){
+    res.send(JSON.stringify(books,null,3));
 });
 
 // Get book details based on ISBN
+
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send("Books with ISBN: " + (isbn) + JSON.stringify(books[isbn],null,3));
  });
   
 // Get book details based on author
+
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
+  res.send("Books found by " + (author) + JSON.stringify(booksByAuthor,null,3));
 });
 
 // Get all books based on title
+
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const booksByTitle = Object.values(books).filter(book => book.title === title)
+    res.send("Books named " + (title) + JSON.stringify(booksByTitle,null,3))
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+public_users.get('/reviews/:reviews',function (req, res) {
+  const reviews = req.params.reviews
+  res.send("Reviews: " + books[reviews])
 });
 
 module.exports.general = public_users;
